5697f089d9bacd6d2a8c1a6ca8bfabfec80a745c
Victoria Esquivel <vee22@att.net>
2015-04-20 23:54:32 -0500

    customized bootstrap font and colors

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get "about" => "pages#about"
+  get "contact_us" => "pages#contact_us"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
